bb17d63b598c85c5d3f0bdd9091227b38856d574
zzhao30 <zzhao30@sheffield.ac.uk>
2021-03-17 02:04:49 +0000

    fixed

--- Huddling Penguins/Huddling Penguins/@penguins/penguins.m
+++ Huddling Penguins/Huddling Penguins/@penguins/penguins.m
@@ -21,7 +21,7 @@ classdef penguins % declares pemguins object
             switch nargin                     %Use switch statement with nargin,varargin contructs to overload constructor methods
                 case 0                        %create default object
                  	p.body_temperature=[];
-                    p.postion=[];
+                    p.pos=[];
                     p.speed=[];
                 case 1                         %input is already a penguin, so just return!
                     if (isa(varargin{1},'penguin'))		
@@ -31,7 +31,7 @@ classdef penguins % declares pemguins object
                     end
                 case 3                          %create a new penguin (currently the only constructor method used)
                   	p.body_temperature =varargin{1};
-                    p.postion=varargin{2};
+                    p.pos=varargin{2};
                     p.speed=varargin{3};       %number of iterations since penguin last reproduced.
                 otherwise
                     error('Invalid no. of input arguments for penguin')
