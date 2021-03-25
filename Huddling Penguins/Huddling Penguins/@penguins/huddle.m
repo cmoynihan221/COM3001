be740f97813e65f28650f7ac9ccebb791d9bd3ec
zzhao30 <zzhao30@sheffield.ac.uk>
2021-03-25 12:20:42 +0000

    change huddle

--- Huddling Penguins/Huddling Penguins/@penguins/huddle.m
+++ Huddling Penguins/Huddling Penguins/@penguins/huddle.m
@@ -1,11 +1,11 @@
 function [agt, closest]= huddle(agt,cn)
 
-    %migration functions for class FOX
-    %agt=fox object
+    %huddle functions for class Penguin
+    %agt= Penguin
     %cn - current agent number
     
-    %SUMMARY OF FOX MIGRATE RULE
-    %If a fox has not eaten, it will pick a random migration direction
+    %SUMMARY OF penguin MIGRATE RULE
+    %If a penguin has not eaten, it will pick a random migration direction
     %If it will leave the edge of the model, the direction is incremented by 45
     %degrees at it will try again (up to 8 times)
     %modified by D Walker 11/4/08
@@ -23,16 +23,16 @@ function [agt, closest]= huddle(agt,cn)
        global  IT_STATS N_IT MESSAGES
    
        pos=agt.pos;                        %extract current position 
-       spd=agt.speed;                      %fox migration speed in units per iteration - this is equal to the food search radius
+       spd=agt.speed;                      %penguin migration speed in units per iteration - this is equal to the food search radius
  
 
        typ = MESSAGES.atype
-       penguins = find(typ==1)                                      %indices of all rabbits
-       ppos=MESSAGES.pos(penguins,:);                                     %extract positions of all rabbits
+       penguins = find(typ==1)                                      %indices of allpenguins
+       ppos=MESSAGES.pos(penguins,:);                                     %extract positions of all penguins
        csep=sqrt((ppos(:,1)-pos(:,1)).^2+(ppos(:,2)-pos(:,2)).^2); 
        csep(cn)= []  %calculate distance to all penguins
-       [d,ind]=min(csep);                                            %d is distance to closest rabbit, ind is index of that rabbit
-        nrst=penguins(ind);                                            %index of nearest rabbit(s)
+       [d,ind]=min(csep);                                            %d is distance to closestpenguin, ind is index of that penguin
+        nrst=penguins(ind);                                            %index of nearestpenguin(s)
        if(MESSAGES.Huddle == true)
            posNrst = MESSAGES.HLoc;
        else
@@ -59,18 +59,18 @@ function [agt, closest]= huddle(agt,cn)
        agt.pos = newPos
        closest = nrst;
        %{
-       if d<=spd&length(nrst)>0    %if there is at least one  rabbit within the search radius        
-           if length(nrst)>1       %if more than one rabbit located at same distance then randomly pick one to head towards
+       if d<=spd&length(nrst)>0    %if there is at least one penguin within the search radius        
+           if length(nrst)>1       %if more than one penguin located at same distance then randomly pick one to head towards
                s=round(rand*(length(nrst)-1))+1;
                nrst=nrst(s);
            end
          
-               nx=MESSAGES.pos(nrst,1);    %extract exact location of this rabbit
+               nx=MESSAGES.pos(nrst,1);    %extract exact location of this penguin
                ny=MESSAGES.pos(nrst,2);
    
                npos=[nx ny];
                
-           if npos(1)<ENV_DATA.bm_size&npos(2)<ENV_DATA.bm_size&npos(1)>=1&npos(2)>=1   %check that fox has not left edge of model - correct if so.
+           if npos(1)<ENV_DATA.bm_size&npos(2)<ENV_DATA.bm_size&npos(1)>=1&npos(2)>=1   %check that penguin has not left edge of model - correct if so.
               agt.pos=npos; 
               IT_STATS.hud(N_IT+1)=IT_STATS.hud(N_IT+1)+1; 
            end
@@ -78,7 +78,7 @@ function [agt, closest]= huddle(agt,cn)
               agt.pos=npos; 
               IT_STATS.hud(N_IT+1)=IT_STATS.hud(N_IT+1)+1; 
                
-               %move agent to position of this rabbit
+               %move agent to position of thispenguin
         
        end
         %}
