9ed66bf07486db588adf65f033b4bee52e9e62cd
acb18hl <381395644@qq.com>
2021-03-25 20:14:43 +0800

    Comment for huddle.m and initialise_result function

--- Huddling Penguins/Huddling Penguins/initialise_results.m
+++ Huddling Penguins/Huddling Penguins/initialise_results.m
@@ -7,11 +7,11 @@ function initialise_results(np,nsteps)
 %iteration (number of agents etc)
 %ENV_DATA - data structure representing the environment 
  
- IT_STATS=struct('div_p',[zeros(1,nsteps+1)],... 
-                'hud',[zeros(1,nsteps+1)],... 
-                'huddleNum',[zeros(1,nsteps+1)],... 
-                'avg_temp',[zeros(1,nsteps+1)],... 
-                'mig',[zeros(1,nsteps+1)],...                %no. agents migrating per iteration
-                'tot',[zeros(1,nsteps+1)],...				%total no. agents in model per iteration
+ IT_STATS=struct('div_p',[zeros(1,nsteps+1)],...    %  
+                'hud',[zeros(1,nsteps+1)],...       %  The huddled penguins per iteration
+                'huddleNum',[zeros(1,nsteps+1)],... %  The numbers of huddled penguins per iteration 
+                'avg_temp',[zeros(1,nsteps+1)],...  %  The average temperature of penguins per iteration
+                'mig',[zeros(1,nsteps+1)],...       %  no. agents migrating per iteration
+                'tot',[zeros(1,nsteps+1)],...	    %  total no. agents in model per iteration
                 'tot_p',[zeros(1,nsteps+1)]); 
 IT_STATS.tot_p(1)=np;
